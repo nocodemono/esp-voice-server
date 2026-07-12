e773840e9e4926f09dd0bf202f1be8350af2ba20
nocodemono <monofei2140@gmail.com>
2026-07-13 01:19:29 +0530

    Add upload endpoint

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return "ESP Voice Server Running!"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if "audio" not in request.files:
+        return "No audio received", 400
+
+    audio = request.files["audio"]
+
+    audio.save("received.wav")
+
+    print("Received:", audio.filename)
+
+    return "UPLOAD OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
